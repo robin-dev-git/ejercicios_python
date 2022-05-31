ff42f14cc22152f45ffc8c127918096980692d7f
robin-dev-git <robin-0507@outlook.com>
2022-05-30 22:02:30 -0500

    Agregar los archivos de pyhon

---
+++ ex001_version.py
@@ -0,0 +1,9 @@
+# Ejercicio 1: Mostrar la versión actual de Python instalada.
+
+import sys
+
+print(sys.version)
+
+print()
+
+print(sys.version_info)
---
+++ ex004_area_circulo.py
@@ -0,0 +1,11 @@
+# Ejericio 4: Solicitar el valor del radio de un círculo para calcular su área.
+
+ # WolframAlpha
+
+from math import pi
+
+r = float(input("Escrib el radio del círculo: ")
+)
+area = pi * r ** 2
+
+print("El área del círculo es {}".format(area))
